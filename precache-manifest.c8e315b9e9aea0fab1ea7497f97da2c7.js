2c0cc5477c41bf975c447188d82d3f3da4e30bcb
Eric Gonzalez <ericegonz.se@gmail.com>
2020-04-02 22:54:24 -0700

    gh pages test

---
+++ precache-manifest.c8e315b9e9aea0fab1ea7497f97da2c7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfa818086ea3f61ad355baa45cbf5414",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b55cb6cb3debfe3f899",
+    "url": "/static/css/2.f681e662.chunk.css"
+  },
+  {
+    "revision": "640321b734696cb0241c",
+    "url": "/static/css/main.1d48c2a6.chunk.css"
+  },
+  {
+    "revision": "8b55cb6cb3debfe3f899",
+    "url": "/static/js/2.c623292d.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.c623292d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "640321b734696cb0241c",
+    "url": "/static/js/main.b8944f5d.chunk.js"
+  },
+  {
+    "revision": "eb94f568bc1b8352756d",
+    "url": "/static/js/runtime-main.5160f881.js"
+  },
+  {
+    "revision": "dd4737cfe090d35668ad29bf95a88175",
+    "url": "/static/media/face.dd4737cf.png"
+  },
+  {
+    "revision": "31e583e3185d7f33754e2f71f4d55d78",
+    "url": "/static/media/prlarge.31e583e3.png"
+  }
+]);
